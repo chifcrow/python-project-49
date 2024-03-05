410af0b74ba8012f038c8df58c392df095b6a457
chifcrow <office@devijen.com>
2024-03-05 22:50:08 +0200

    Add user greeting in brain games

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -12,3 +12,5 @@
 Makefile,8\3\836efb6e25a091dcb4ff8e1dbb2f0be6a5cbf14c
 R
 "brain_games/scripts/brain_games.py,b\0\b0147285bc909b9ec6adf60f60a85305030de1c3
+F
+brain_games/cli/cli.py,f\3\f372c2a81f78d9fc982335b6f358640c833932c2
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 # brain_games/scripts/brain_games.py
 
+from brain_games.cli.cli import welcome_user
+
 def main():
-    print("Welcome to the Brain Games!")
+    welcome_user()
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
