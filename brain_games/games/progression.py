179a212eeb8f8690e91c94520ef39a8faf86b438
chifcrow <office@devijen.com>
2024-03-13 19:40:52 +0200

    Refactoring progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -3,14 +3,16 @@ import random
 
 
 def generate_question_answer():
-    start = random.randint(1, 10)
-    step = random.randint(1, 5)
-    length = random.randint(5, 10)
-    hidden_index = random.randint(0, length - 1)
+    print("What number is missing in the progression?")  # Добавляем это уведомление
 
-    progression = [str(start + i * step) for i in range(length)]
-    correct_answer = progression[hidden_index]
-    progression[hidden_index] = '..'
+    progression_length = 10
+    start = random.randint(1, 10)
+    step = random.randint(1, 10)
+    missing_index = random.randint(0, progression_length - 1)
+
+    progression = [str(start + step * i) for i in range(progression_length)]
+    correct_answer = progression[missing_index]
+    progression[missing_index] = '..'
     question = ' '.join(progression)
 
     return question, correct_answer
