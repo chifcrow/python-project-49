17b99b3345f58a7e3e9e2f41c23bba40b6540739
chifcrow <office@devijen.com>
2024-03-13 19:19:41 +0200

    Refactoring gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -1,10 +1,16 @@
 # brain_games/games/gcd.py
 import random
-import math
 
 def generate_question_answer():
+    print("Find the greatest common divisor of given numbers.")  # Добавление описания игры
+
     num1 = random.randint(1, 100)
     num2 = random.randint(1, 100)
-    question = f'{num1} {num2}'
-    answer = str(math.gcd(num1, num2))
-    return question, answer
+    question = f"{num1} {num2}"
+    correct_answer = str(find_gcd(num1, num2))
+    return question, correct_answer
+
+def find_gcd(num1, num2):
+    while num2:
+        num1, num2 = num2, num1 % num2
+    return num1
