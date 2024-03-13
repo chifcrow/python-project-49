a057b249085653c3af8c6d84875c457caa71f84e
chifcrow <office@devijen.com>
2024-03-13 23:04:53 +0200

    Refactoring_2 prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -3,7 +3,7 @@ import random
 
 
 def is_prime(number):
-    if number < 2:
+    if number <= 1:
         return False
     for i in range(2, int(number ** 0.5) + 1):
         if number % i == 0:
@@ -12,9 +12,8 @@ def is_prime(number):
 
 
 def generate_question_answer():
-    print("Answer 'yes' if given number is prime. Otherwise answer 'no'.")  # Добавление описания игры
-
     number = random.randint(1, 100)
+    print('Answer "yes" if given number is prime. Otherwise answer "no".')
     question = str(number)
     correct_answer = 'yes' if is_prime(number) else 'no'
     return question, correct_answer
